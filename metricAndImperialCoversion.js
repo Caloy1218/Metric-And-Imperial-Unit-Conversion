8aea47d419e19ff13ab3669b988834745543d589
John Carlo Saquing <112403931+Caloy1218@users.noreply.github.com>
2022-11-03 15:52:58 +0800

    Add files via upload

---
+++ metricAndImperialCoversion.js
@@ -0,0 +1,36 @@
+let inputNum = document.querySelector("#inputNum");
+let convertBtn = document.querySelector("#convertBtn");
+let length = document.querySelector("#length");
+let volumeEl = document.querySelector("#volume1");
+let mass = document.querySelector("#mass");
+const feet = 3.281;
+const gallon = 0.264;
+const pound = 2.204;
+convertBtn.addEventListener("click", function () {
+  lengthConversion();
+  volumeConversion();
+  massConversion();
+});
+
+function lengthConversion() {
+  let meter = inputNum.value * feet;
+  let feetVal = inputNum.value / feet;
+  length.innerHTML = `${inputNum.value} meters = ${meter.toFixed(3)}feet | ${
+    inputNum.value
+  } feet = ${feetVal.toFixed(3)}meters`;
+}
+
+function volumeConversion() {
+  let liters = inputNum.value * gallon;
+  let gallons = inputNum.value / gallon;
+  volumeEl.innerHTML = `${inputNum.value} liters = ${liters.toFixed(
+    3
+  )}gallons | ${inputNum.value} gallons = ${gallons.toFixed(3)}liters `;
+}
+function massConversion() {
+  let kilos = inputNum.value * pound;
+  let pounds = inputNum.value / pound;
+  mass.innerHTML = `${inputNum.value} kilos  = ${kilos.toFixed(3)}pounds | ${
+    inputNum.value
+  } pounds = ${pounds.toFixed(3)}kilos `;
+}
