fefe1ed9b13114c78c14c786e5b88c3d131eff74
John Carlo Saquing <112403931+Caloy1218@users.noreply.github.com>
2022-11-16 21:43:54 +0800

    new com

--- metricAndImperialCoversion.js
+++ metricAndImperialCoversion.js
@@ -6,6 +6,9 @@ let mass = document.querySelector("#mass");
 const feet = 3.281;
 const gallon = 0.264;
 const pound = 2.204;
+const kgLbs = document.querySelector("#kgLbs");
+const LG = document.querySelector("#litersGallon");
+const mF = document.querySelector("#meterFeet");
 convertBtn.addEventListener("click", function () {
   lengthConversion();
   volumeConversion();
@@ -15,6 +18,7 @@ convertBtn.addEventListener("click", function () {
 function lengthConversion() {
   let meter = inputNum.value * feet;
   let feetVal = inputNum.value / feet;
+  mF.innerHTML = `Length(Meter/Feet)`;
   length.innerHTML = `${inputNum.value} meters = ${meter.toFixed(3)}feet | ${
     inputNum.value
   } feet = ${feetVal.toFixed(3)}meters`;
@@ -23,6 +27,7 @@ function lengthConversion() {
 function volumeConversion() {
   let liters = inputNum.value * gallon;
   let gallons = inputNum.value / gallon;
+  LG.innerHTML = ` Volume(Liters/Gallons)`;
   volumeEl.innerHTML = `${inputNum.value} liters = ${liters.toFixed(
     3
   )}gallons | ${inputNum.value} gallons = ${gallons.toFixed(3)}liters `;
@@ -30,6 +35,7 @@ function volumeConversion() {
 function massConversion() {
   let kilos = inputNum.value * pound;
   let pounds = inputNum.value / pound;
+  kgLbs.innerHTML = `Mass(Kilograms/Pounds)`;
   mass.innerHTML = `${inputNum.value} kilos  = ${kilos.toFixed(3)}pounds | ${
     inputNum.value
   } pounds = ${pounds.toFixed(3)}kilos `;
